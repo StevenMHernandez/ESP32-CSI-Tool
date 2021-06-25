3a91fcf28da759d27ef9b0d447c7b3269429ad16
StevenMHernandez <me@stevenmhernandez.com>
2021-06-25 10:35:19 -0400

    replace `make menuconfig` with `idf.py menuconfig` for v4 compatibility

--- active_ap/main/main.cc
+++ active_ap/main/main.cc
@@ -21,7 +21,7 @@
 #include "../../_components/sockets_component.h"
 
 /*
- * The examples use WiFi configuration that you can set via 'make menuconfig'.
+ * The examples use WiFi configuration that you can set via 'idf.py menuconfig'.
  *
  * If you'd rather not, just change the below entries to strings with
  * the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
