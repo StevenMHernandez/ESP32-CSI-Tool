bfd4ab19f2443b72c02db74f2c968c54f0355e5d
StevenMHernandez <me@stevenmhernandez.com>
2022-01-07 12:41:19 -0500

    remove deprecation warnings (`esp_event_*` and `tcpip_*`) for esp-idf v4.3

--- active_ap/main/main.cc
+++ active_ap/main/main.cc
@@ -5,7 +5,7 @@
 #include "esp_spi_flash.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
@@ -65,38 +65,34 @@ static EventGroupHandle_t s_wifi_event_group;
 
 static const char *TAG = "Active CSI collection (AP)";
 
-static esp_err_t event_handler(void *ctx, system_event_t *event) {
-    switch (event->event_id) {
-        case SYSTEM_EVENT_AP_STACONNECTED:
-            ESP_LOGI(TAG, "station:"
-            MACSTR
-            " join, AID=%d",
-                    MAC2STR(event->event_info.sta_connected.mac),
-                    event->event_info.sta_connected.aid);
-            break;
-        case SYSTEM_EVENT_AP_STADISCONNECTED:
-            ESP_LOGI(TAG, "station:"
-            MACSTR
-            "leave, AID=%d",
-                    MAC2STR(event->event_info.sta_disconnected.mac),
-                    event->event_info.sta_disconnected.aid);
-            break;
-        default:
-            break;
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
     }
-    return ESP_OK;
 }
 
 void softap_init() {
     s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
-
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-//    wifi_config_t wifi_config = {};
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
     wifi_ap_config_t wifi_ap_config = {};
     wifi_ap_config.channel = WIFI_CHANNEL;
     wifi_ap_config.authmode = WIFI_AUTH_WPA_WPA2_PSK;
--- active_sta/main/main.cc
+++ active_sta/main/main.cc
@@ -5,7 +5,7 @@
 #include "esp_spi_flash.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "esp_http_client.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
@@ -91,25 +91,19 @@ esp_err_t _http_event_handle(esp_http_client_event_t *evt) {
 //// en_sys_seq: see https://github.com/espressif/esp-idf/blob/master/docs/api-guides/wifi.rst#wi-fi-80211-packet-send for details
 esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
 
-static esp_err_t event_handler(void *ctx, system_event_t *event) {
-    switch (event->event_id) {
-        case SYSTEM_EVENT_STA_START:
-            esp_wifi_connect();
-            break;
-        case SYSTEM_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "Got ip: %s", ip4addr_ntoa((const ip4_addr_t *) &event->event_info.got_ip.ip_info.ip));
-            xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-            break;
-        case SYSTEM_EVENT_STA_DISCONNECTED: {
-            ESP_LOGI(TAG, "Retry connecting to the AP");
-            esp_wifi_connect();
-            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-            break;
-        }
-        default:
-            break;
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGI(TAG, "Retry connecting to the AP");
+        esp_wifi_connect();
+        xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
-    return ESP_OK;
 }
 
 bool is_wifi_connected() {
@@ -119,15 +113,26 @@ bool is_wifi_connected() {
 void station_init() {
     s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_netif_init());
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
 
     wifi_sta_config_t wifi_sta_config = {};
     wifi_sta_config.channel = WIFI_CHANNEL;
